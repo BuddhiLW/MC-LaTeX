f4387750e2ce723f7967e47017df81216170ba90
BuddhiLW <59554283+26-55-87-BuddhiLW@users.noreply.github.com>
2020-01-24 20:40:49 -0300

    Update texinstall.sh
    Formatação visual da instalação no bash

--- texinstall.sh
+++ texinstall.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
-
-echo "Hello! Namaste, milady..."
-echo "This is a bit weird, isn't is?"
-echo "Updating and Upgrading your packages"
+echo "Saudações!"
+echo $'Scripts são tão úteis!\n'
+#echo "Updating and Upgrading your packages"
 #sudo apt-get update && sudo apt-get upgrade
-echo "Downloading texlive full package and texstudio"
-sudo apt-get install --allow texlive-full texstudio --yes
-echo "Now, let's remove any unnecessary packages that are installed"
-sudo apt-get autoremove
-echo "Ok, now you can use LaTeX in your favorite IDE"
+#echo "Downloading texlive full package and texstudio"
+echo $'Será feita a instalação completa do LaTeX.\n'
+sudo apt-get install texlive-full texstudio --yes
+echo $'\n'
+echo $'Instalaremos, também, o TeXstudio (IDE pra LaTeX)\n'
+sudo apt-get install texstudio
+#echo "Now, let's remove any unnecessary packages that are installed"
+#sudo apt-get autoremove
+#echo "Ok, now you can use LaTeX in your favorite IDE"
+echo $'\n'
+echo $'Agora, é só explorar o LaTeX na sua IDE preferida :) \n'
